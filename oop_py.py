cae136670a3213ab2a69e36ab9b3bc635dc1259c
Teatoller <Steven.otieno1@gmail.com>
2018-09-08 15:09:45 +0300

    Add new object attributes

--- oop_py.py
+++ oop_py.py
@@ -5,10 +5,13 @@
 
 class Dog:
 
-    def __init__(self):
-        pass
+    def __init__(self, name, age):  # Add new attributes to object
+
+        self.name = name
+        self.age = age
 
 
-# Intantiate an object
-spotty = Dog()
-print(spotty)
+# Instantiate an object
+spotty = Dog("Spotty", 3)
+print(spotty.name)
+print(spotty.age)
