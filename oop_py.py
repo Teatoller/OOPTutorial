80ee9b763d04faeb0b11077265ae3a17eab720d0
Teatoller <Steven.otieno1@gmail.com>
2018-09-08 16:09:06 +0300

    Add pairing method

--- oop_py.py
+++ oop_py.py
@@ -21,18 +21,32 @@ class Dog:
     def birthday(self):
         self.age += 1
 
+# pal is a dog which is a friend to Dog
+    def setPal(self, pal):
+        self.pal = pal
+        pal.pal = self
+
 
 # Instantiate an object
 spotty = Dog("Spotty", 3)
 tunker = Dog("Tunker", 10)
 harrier = Dog("Harrier", 1)
 
+spotty.setPal(tunker)
 # spotty.bark()
 # print(spotty.name)
+
 
 spotty.birthday()
 tunker.birthday()
 
+
 spotty.doginfo()
 tunker.doginfo()
 harrier.doginfo()
+
+print(spotty.pal.name)
+print(spotty.pal.age)
+
+print(tunker.pal.name)
+print(tunker.pal.age)
