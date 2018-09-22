5da33cf8d58f82f037195c3f4d4d747a4272c161
Teatoller <Steven.otieno1@gmail.com>
2018-09-22 13:16:59 +0300

    Handle mixed Fractions

--- OOPClass/classFraction.py
+++ OOPClass/classFraction.py
@@ -6,10 +6,25 @@ class Fraction:
         self.num = top
         self.den = bottom
 
-    def show(self):
-        print(self.num, '/', self.den)
+    def __str__(self):
+        return str(self.num) + '/' + str(self.den)
+
+    def __add__(self, otherfraction):
+
+        newnum = self.num * otherfraction.den + self.den * otherfraction.num
+        newden = self.den * otherfraction.den
+
+        return Fraction(newnum, newden)
 
 
 myfraction = Fraction(3, 5)
-myfraction.show()
+
 print(myfraction)
+print("I ate", myfraction, "of the pizza")
+print(myfraction.__str__())
+print(str(myfraction))
+
+f1 = Fraction(1, 4)
+f2 = Fraction(1, 2)
+g = (f1).__add__(f2)
+print(g)
